3560dce8fe6dbcfeb146ec1ae6b6f6ebb22b32e4
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-10-15 09:44:57 -0700

    Fix Logging

--- src/main/java/io/gamer4life1/simplelogistics/SimpleLogistics.java
+++ src/main/java/io/gamer4life1/simplelogistics/SimpleLogistics.java
@@ -2,7 +2,12 @@ package io.gamer4life1.simplelogistics;
 import io.gamer4life1.simplelogistics.proxy.CommonProxy;
 import io.gamer4life1.simplelogistics.util.Reference;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -10,6 +15,9 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
@@ -22,12 +30,12 @@ public class SimpleLogistics
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.COMMON_PROXY_CLASS)
     public static CommonProxy proxy;
 
-    private static Logger logger;
+    private static Logger logger = LogManager.getLogger();
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
+
     }
 
     @EventHandler
@@ -41,4 +49,12 @@ public class SimpleLogistics
     {
 
     }
+
+    @SubscribeEvent
+    public void leftClick(PlayerInteractEvent.LeftClickBlock event) {
+        ItemStack itemStack = event.getEntityPlayer().getHeldItem(event.getHand());
+        logger.info(itemStack.getDisplayName());
+        IBlockState state = event.getWorld().getBlockState(event.getPos());
+    }
+
 }
