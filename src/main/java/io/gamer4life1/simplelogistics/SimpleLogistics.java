f645e8158bf3fd1666808e2278e8b83251f706cb
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-10-14 20:19:38 -0700

    fix naming, etc, etc

---
+++ src/main/java/io/gamer4life1/simplelogistics/SimpleLogistics.java
@@ -0,0 +1,31 @@
+package io.gamer4life1.simplelogistics;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = SimpleLogistics.MODID, name = SimpleLogistics.NAME, version = SimpleLogistics.VERSION)
+public class SimpleLogistics
+{
+    public static final String MODID = "simplelogistics";
+    public static final String NAME = "Simple Logistics";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
